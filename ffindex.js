ddf32c45ef561b417f4746925154427735697b3d
Roli <rroycehg@gmail.com>
2021-06-02 04:42:05 -0400

    Some info display

--- ffindex.js
+++ ffindex.js
@@ -7,23 +7,31 @@ fetch(ffUrl)
 .then(res => res.json())
 .then(characters => {
     listChar(characters)
-
+    appendChar(characters)
 })
 
 
 //Get the Array of each Char-info block using for-loop
 function listChar(char){
-        let array = Object(char);
-        
-       for (i = 0; i < Object(char).length; i++){
-             array = Object(char)[i];
+    let ul = document.querySelector(`#charactersList`)   
+    let arrayName = [];
+    let arrayDescrip = [];
+    for (i = 0; i < Object(char).length; i++){
+             arrayName = Object(char)[i].name
+             arrayDescrip = Object(char)[i].description
              //console.log(array)
-        }
+             let h2 = document.createElement(`h2`)
+             let pTags = document.createElement(`p`)
+             pTags.textContent = arrayDescrip
+             h2.textContent = arrayName
+             ul.append(h2)
+             ul.append(pTags)         
+    }
 }
 
 //Take the list of Characters and append it to a container
 function appendChar() {
-
+    
 }
 
 // add eventlistener -click to move to different character

f344dd590020f1dd1bab31f48cf558c44692b160
Darren <DarrenLeel33@gmail.com>
2021-06-03 00:17:36 -0400

    draft

--- ffindex.js
+++ ffindex.js
@@ -2,6 +2,13 @@
 const ffUrl = 'https://www.moogleapi.com/api/v1/characters'
 listChar();
 
+let ul = document.querySelector(`#charactersList`)   
+let searchBar = document.getElementById(`#searchbar`)   
+let arrayName = [];
+let arrayDescrip = [];
+let arrayPicture = [];
+
+
 // fetch from FF DB
 fetch(ffUrl)
 .then(res => res.json())
@@ -13,37 +20,43 @@ fetch(ffUrl)
 
 //Get the Array of each Char-info block using for-loop
 function listChar(char){
-    let ul = document.querySelector(`#charactersList`)   
-    let arrayName = [];
-    let arrayDescrip = [];
-    let pictureArray = [];
-    let picUrlArray =[];
+   
     
     for (i = 0; i < Object(char).length; i++){
              arrayName = Object(char)[i].name
              arrayDescrip = Object(char)[i].description
-            // pictureArray =Object(char)[i].pictures
-             console.log(char)
+             arrayPicture =Object(char)[i].pictures[0].url
              let h2 = document.createElement(`h2`)
              let pTags = document.createElement(`p`)
              let imgTag= document.createElement('img')
              pTags.textContent = arrayDescrip
              h2.textContent = arrayName
-             imgTag.src=pictureArray.url
+             imgTag.src=arrayPicture
              ul.append(h2)
              ul.append(pTags)         
              ul.append(imgTag)
              
              //picture url
-     
+            
             
             }
         }
-function pullUrl(url){
 
-}
+          
 
 //Take the list of Characters and append it to a container
 
 
 // add eventlistener -click to move to different characters
+
+
+
+
+
+
+searchBar.addEventlistener(keyup, (e)=>{
+    let input=e.target.value 
+    console.log(input)
+} )
+
+

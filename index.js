8e1edccac62fd498542e0bb715dcf417d46321f2
Darren <DarrenLeel33@gmail.com>
2021-06-04 09:02:10 -0400

    draft

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const ffUrl = 'https://www.moogleapi.com/api/v1/characters'
 
 let ul = document.querySelector('.rightColumn') 
 
+
+
+
+
+
+
+
+
+
+
+
+
 // fetch from FF DB
 fetch(ffUrl)
 .then(res => res.json())
@@ -16,44 +28,67 @@ fetch(ffUrl)
 function listName(name){
     name.forEach(nameObj=>{
         let ffname =nameObj.name
-       
         let li = document.createElement("li")
         li.textContent=ffname
         ul.append(li)
+        li.addEventListener("click", () => li.style.color = "#ce42f5" )
         
+        text.onmouseover = changeName () =>{
+        //change to japanese name
+            let charName = element.japaneseName
+        let h2= document.querySelector(".name")
+        h2.textContent= charName
+
+        }
         
     })
 }
 
+//Event Handler
+
+
+
+
+
+
+//DISPLAY FUNCTIONS
+//
 //grabs name to display banner
 function displayName(bannerName){
     bannerName.forEach(element=>{
         let charName = element.name
         let h2= document.querySelector(".name")
         h2.textContent= charName
-        
     })
+
+
+        
+    
     
 }
 
 //grabs desciption to display banner
 function displayDs(ds){
-    ds.forEach(element=>{
+   ds.forEach(element=>{
         let descpt = document.querySelector(".descpt")
         let charDes = element.description
         descpt.textContent=charDes 
         
-    })
+})
     
 }
 
+
+
+
 // grabs picture to display banner
 function displayPicture(picture){
-    picture.forEach(element=>{
+   picture.forEach(element=>{
         let img= document.querySelector("img")
         let charPic = element.pictures[0].url
         img.src=charPic 
         
-    })
+})
 
 }
+

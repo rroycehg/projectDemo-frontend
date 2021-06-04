e73469492d8b271ba0349645fe703a74da998185
Darren <DarrenLeel33@gmail.com>
2021-06-04 16:07:22 -0400

    final

--- index.js
+++ index.js
@@ -4,24 +4,13 @@ let ul = document.querySelector('.rightColumn')
 
 
 
-
-
-
-
-
-
-
-
-
-
 // fetch from FF DB
 fetch(ffUrl)
 .then(res => res.json())
 .then(chars => {
     listName(chars)
-    displayName(chars)
-    displayDs(chars)
-    displayPicture(chars)
+  
+    
 })
 .catch(error => {console.log(error);})
 
@@ -31,64 +20,62 @@ function listName(name){
         let li = document.createElement("li")
         li.textContent=ffname
         ul.append(li)
-        li.addEventListener("click", () => li.style.color = "#ce42f5" )
         
-        text.onmouseover = changeName () =>{
-        //change to japanese name
-            let charName = element.japaneseName
-        let h2= document.querySelector(".name")
-        h2.textContent= charName
+        
+        //Event Handler
+        li.addEventListener("click", (e) =>{
+            console.log(e.target.innerText);
+            li.style.color = "#ce42f5" 
+            displayName();
+            
 
-        }
+        // DISPLAY FUNCTIONS
+        
+        // grabs name to display banner
+        function displayName(bannerName){
+   
+            bannerName = e.target.innerText
+            let h2= document.querySelector(".name")
+            h2.textContent= e.target.innerText
+
+            fetch(ffUrl)
+                .then((res) => res.json())
+                .then((data) => {
+                    let descpt = document.querySelector(".descpt")
+                    let charPic = nameObj.pictures[0].url
+                    let img= document.querySelector("img")
+                    if (bannerName=nameObj.name) {
+                        descpt.textContent= nameObj.description
+                        img.src=charPic 
+                        
+                    }
+                })
+                
+                
+            }
+            
+            
+        
+
+
+
+
+
+
+
+        })
+        
+        
         
     })
 }
 
-//Event Handler
 
 
-
-
-
-
-//DISPLAY FUNCTIONS
-//
-//grabs name to display banner
-function displayName(bannerName){
-    bannerName.forEach(element=>{
-        let charName = element.name
-        let h2= document.querySelector(".name")
-        h2.textContent= charName
-    })
-
-
-        
     
-    
-}
-
-//grabs desciption to display banner
-function displayDs(ds){
-   ds.forEach(element=>{
-        let descpt = document.querySelector(".descpt")
-        let charDes = element.description
-        descpt.textContent=charDes 
-        
-})
-    
-}
+   
 
 
 
 
-// grabs picture to display banner
-function displayPicture(picture){
-   picture.forEach(element=>{
-        let img= document.querySelector("img")
-        let charPic = element.pictures[0].url
-        img.src=charPic 
-        
-})
-
-}
 

1db6d2988e3a7dba41eb7eedf27a4120d96f5186
Roli <rroycehg@gmail.com>
2021-06-04 16:14:14 -0400

    Completed Project-frontend

--- index.js
+++ index.js
@@ -1,72 +1,41 @@
 const ffUrl = 'https://www.moogleapi.com/api/v1/characters'
-
 let ul = document.querySelector('.rightColumn') 
-
 // fetch from FF DB
 fetch(ffUrl)
 .then(res => res.json())
 .then(chars => {
     listName(chars)
-    displayName(chars)
-    displayDs(chars)
-    displayPicture(chars)
 })
 .catch(error => {console.log(error);})
-
-
-//Find target and add an event listener for updating Reviews
-
-const searchInput = document.getElementById(`search`)
-
-searchInput.addEventListener(`input`, (event) => {
-  const userInput = event.target.value
-
-  
-})
-
-
-
 function listName(name){
     name.forEach(nameObj=>{
         let ffname =nameObj.name
-       
         let li = document.createElement("li")
         li.textContent=ffname
         ul.append(li)
-        
-        
+        //Event Handler
+        li.addEventListener("click", (e) =>{
+            console.log(e.target.innerText);
+            li.style.color = "#ce42f5" 
+            displayName();
+        // DISPLAY FUNCTIONS
+        // grabs name to display banner
+        function displayName(bannerName){
+            bannerName = e.target.innerText
+            let h2= document.querySelector(".name")
+            h2.textContent= e.target.innerText
+            fetch(ffUrl)
+                .then((res) => res.json())
+                .then((data) => {
+                    let descpt = document.querySelector(".descpt")
+                    let charPic = nameObj.pictures[0].url
+                    let img= document.querySelector("img")
+                    if (bannerName=nameObj.name) {
+                        descpt.textContent= nameObj.description
+                        img.src=charPic 
+                    }
+                })
+            }
+        })
     })
-}
-
-//grabs name to display banner
-function displayName(bannerName){
-    bannerName.forEach(element=>{
-        let charName = element.name
-        let h2= document.querySelector(".name")
-        h2.textContent= charName
-        
-    })
-    
-}
-
-//grabs desciption to display banner
-function displayDs(ds){
-    ds.forEach(element=>{
-        let descpt = document.querySelector(".descpt")
-        let charDes = element.description
-        descpt.textContent=charDes 
-        
-    })
-    
-}
-
-// grabs picture to display banner
-function displayPicture(picture){
-    picture.forEach(element=>{
-        let img= document.querySelector("img")
-        let charPic = element.pictures[0].url
-        img.src=charPic 
-        
-    })
-
 }
